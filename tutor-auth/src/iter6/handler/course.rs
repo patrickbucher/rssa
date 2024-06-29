f19f5cfb2e327fcf5e99998ad97fc13cf22f4d93
Patrick Bucher <patrick.bucher@mailbox.org>
2024-06-29 16:30:13 +0200

    finished yet another mind-bogglig section

--- tutor-auth/src/iter6/handler/course.rs
+++ tutor-auth/src/iter6/handler/course.rs
@@ -1,20 +1,67 @@
 use crate::iter6::state::AppState;
+use crate::model::{NewCourse, NewCourseResponse, UpdateCourse, UpdateCourseResponse};
 use actix_web::{web, Error, HttpResponse, Result};
+use serde_json::json;
 
 pub async fn handle_insert_course(
     _tmpl: web::Data<tera::Tera>,
     _app_state: web::Data<AppState>,
+    path: web::Path<i32>,
+    params: web::Json<NewCourse>,
 ) -> Result<HttpResponse, Error> {
-    println!("Got insert request");
-    Ok(HttpResponse::Ok().body("Got insert request"))
+    let tutor_id = path.into_inner();
+    let new_course = json!({
+        "tutor_id": tutor_id,
+        "course_name": &params.course_name,
+        "course_description": &params.course_description,
+        "course_format": &params.course_format,
+        "course_structure": &params.course_structure,
+        "course_duration": &params.course_duration,
+        "course_price": &params.course_price,
+        "course_language": &params.course_language,
+        "course_level": &params.course_level
+    });
+    let awc_client = awc::Client::default();
+    let res = awc_client
+        .post("http://localhost:3000/courses/")
+        .send_json(&new_course)
+        .await
+        .unwrap()
+        .body()
+        .await?;
+    println!("Finished call: {:?}", res);
+    let course_response: NewCourseResponse = serde_json::from_str(&std::str::from_utf8(&res)?)?;
+    Ok(HttpResponse::Ok().json(course_response))
 }
 
 pub async fn handle_update_course(
     _tmpl: web::Data<tera::Tera>,
     _app_state: web::Data<AppState>,
+    path: web::Path<(i32, i32)>,
+    params: web::Json<UpdateCourse>,
 ) -> Result<HttpResponse, Error> {
-    println!("Got update request");
-    Ok(HttpResponse::Ok().body("Got update request"))
+    let (tutor_id, course_id) = path.into_inner();
+    let update_course = json!({
+        "course_name": &params.course_name,
+        "course_description": &params.course_description,
+        "course_format": &params.course_format,
+        "course_duration": &params.course_duration,
+        "course_structure": &params.course_structure,
+        "course_price": &params.course_price,
+        "course_language": &params.course_language,
+        "course_level": &params.course_level
+    });
+    let awc_client = awc::Client::default();
+    let update_url = format!("http://localhost:3000/courses/{}/{}", tutor_id, course_id);
+    let res = awc_client
+        .put(update_url)
+        .send_json(&update_course)
+        .await
+        .unwrap()
+        .body()
+        .await?;
+    let course_response: UpdateCourseResponse = serde_json::from_str(&std::str::from_utf8(&res)?)?;
+    Ok(HttpResponse::Ok().json(course_response))
 }
 
 pub async fn handle_delete_course(
