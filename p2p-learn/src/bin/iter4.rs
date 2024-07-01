ee1226df358329929b1f48f9af19156eb6a9d5a0
Patrick Bucher <patrick.bucher@mailbox.org>
2024-07-01 06:52:28 +0200

    chapter 11

---
+++ p2p-learn/src/bin/iter4.rs
@@ -0,0 +1,39 @@
+use libp2p::{
+    futures::StreamExt,
+    identity,
+    mdns::{Mdns, MdnsConfig, MdnsEvent},
+    swarm::{Swarm, SwarmEvent},
+    PeerId,
+};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let new_key = identity::Keypair::generate_ed25519();
+    let peer_id = PeerId::from(new_key.public());
+    println!("local peer id is: {:?}", peer_id);
+
+    let transport = libp2p::development_transport(new_key).await?;
+    let behaviour = Mdns::new(MdnsConfig::default()).await?;
+    let mut swarm = Swarm::new(transport, behaviour, peer_id);
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => {
+                println!("Listening on local address {:?}", address)
+            }
+            SwarmEvent::Behaviour(MdnsEvent::Discovered(peers)) => {
+                for (peer, addr) in peers {
+                    println!("discovered {} {}", peer, addr);
+                }
+            }
+            SwarmEvent::Behaviour(MdnsEvent::Expired(expired)) => {
+                for (peer, addr) in expired {
+                    println!("expired {} {}", peer, addr);
+                }
+            }
+            _ => {}
+        }
+    }
+}
